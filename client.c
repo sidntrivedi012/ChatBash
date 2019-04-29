72b04609d4453b89934aebbb50ce48262f712914
nightwarrior-xxx <amanv1999@gmail.com>
2019-04-29 19:16:06 +0530

    Initialisation of client side script

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+	return 0;
+}
