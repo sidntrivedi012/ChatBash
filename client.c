94d7ee91398a47518a919ef8bbd076ad990360c2
code-monk08 <mayank.singh081997@gmail.com>
2019-05-02 00:23:57 +0530

    upstream

--- client.c
+++ client.c
@@ -1,82 +1,353 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+/*
+*
+* Chatroom - a simple linux commandline client/server C program for group chat.
+* Author: Andrew Herriot
+* License: Public Domain
+*
+*/
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "chatroom_utils.h"
+
+
+void trim_newline(char *text)
+{
+  int len = strlen(text) - 1;
+  if (text[len] == '\n')
+{
+      text[len] = '\0';
+  }
+}
+
+void clear_stdin_buffer()
+{
+  int c;
+  while((c = getchar()) != '\n' && c != EOF)
+    /* discard content*/ ;
+}
+
+// get a username from the user.
+void get_username(char *username)
+{
+  while(true)
+  {
+    printf("Enter a username: ");
+    fflush(stdout);
+    memset(username, 0, 1000);
+    fgets(username, 22, stdin);
+    trim_newline(username);
+
+    if(strlen(username) > 20)
+    {
+      // clear_stdin_buffer();
+      puts("Username must be 20 characters or less.");
+
+    } else {
+      break;
+    }
+  }
+}
+
+//send local username to the server.
+void set_username(connection_info *connection)
+{
+  message msg;
+  msg.type = SET_USERNAME;
+  strncpy(msg.username, connection->username, 20);
+
+  if(send(connection->socket, (void*)&msg, sizeof(msg), 0) < 0)
+  {
+    perror("Send failed");
+    exit(1);
+  }
+}
+
+void stop_client(connection_info *connection)
+{
+  close(connection->socket);
+  exit(0);
+}
+
+//initialize connection to the server.
+void connect_to_server(connection_info *connection, char *address, char *port)
+{
+
+  while(true)
+  {
+    get_username(connection->username);
+
+    //Create socket
+    if ((connection->socket = socket(AF_INET, SOCK_STREAM , IPPROTO_TCP)) < 0)
+    {
+        perror("Could not create socket");
+    }
+
+    connection->address.sin_addr.s_addr = inet_addr(address);
+    connection->address.sin_family = AF_INET;
+    connection->address.sin_port = htons(atoi(port));
+
+    //Connect to remote server
+    if (connect(connection->socket, (struct sockaddr *)&connection->address , sizeof(connection->address)) < 0)
+    {
+        perror("Connect failed.");
+        exit(1);
+    }
+
+    set_username(connection);
+
+    message msg;
+    ssize_t recv_val = recv(connection->socket, &msg, sizeof(message), 0);
+    if(recv_val < 0)
+    {
+        perror("recv failed");
+        exit(1);
+
+    }
+    else if(recv_val == 0)
+    {
+      close(connection->socket);
+      printf("The username \"%s\" is taken, please try another name.\n", connection->username);
+      continue;
+    }
+
+    break;
+  }
+
+
+  puts("Connected to server.");
+  puts("Type /help for usage.");
+}
+
+
+void handle_user_input(connection_info *connection)
+{
+  char input[255];
+  fgets(input, 255, stdin);
+  trim_newline(input);
+
+  if(strcmp(input, "/q") == 0 || strcmp(input, "/quit") == 0)
+  {
+    stop_client(connection);
+  }
+  else if(strcmp(input, "/l") == 0 || strcmp(input, "/list") == 0)
+  {
+    message msg;
+    msg.type = GET_USERS;
+
+    if(send(connection->socket, &msg, sizeof(message), 0) < 0)
+    {
+        perror("Send failed");
+        exit(1);
+    }
+  }
+  else if(strcmp(input, "/h") == 0 || strcmp(input, "/help") == 0)
+  {
+    puts("/quit or /q: Exit the program.");
+    puts("/help or /h: Displays help information.");
+    puts("/list or /l: Displays list of users in chatroom.");
+    puts("/m <username> <message> Send a private message to given username.");
+  }
+  else if(strncmp(input, "/m", 2) == 0)
+  {
+    message msg;
+    msg.type = PRIVATE_MESSAGE;
+
+    char *toUsername, *chatMsg;
+
+    toUsername = strtok(input+3, " ");
+
+    if(toUsername == NULL)
+    {
+      puts(KRED "The format for private messages is: /m <username> <message>" RESET);
+      return;
+    }
+
+    if(strlen(toUsername) == 0)
+    {
+      puts(KRED "You must enter a username for a private message." RESET);
+      return;
+    }
+
+    if(strlen(toUsername) > 20)
+    {
+      puts(KRED "The username must be between 1 and 20 characters." RESET);
+      return;
+    }
+
+    chatMsg = strtok(NULL, "");
+
+    if(chatMsg == NULL)
+    {
+      puts(KRED "You must enter a message to send to the specified user." RESET);
+      return;
+    }
+
+    //printf("|%s|%s|\n", toUsername, chatMsg);
+    strncpy(msg.username, toUsername, 20);
+    strncpy(msg.data, chatMsg, 255);
+
+    if(send(connection->socket, &msg, sizeof(message), 0) < 0)
+    {
+        perror("Send failed");
+        exit(1);
+    }
+
+  }
+  else //regular public message
+  {
+    message msg;
+    msg.type = PUBLIC_MESSAGE;
+    strncpy(msg.username, connection->username, 20);
+
+    // clear_stdin_buffer();
+
+    if(strlen(input) == 0) {
+        return;
+    }
+
+    strncpy(msg.data, input, 255);
+
+    //Send some data
+    if(send(connection->socket, &msg, sizeof(message), 0) < 0)
+    {
+        perror("Send failed");
+        exit(1);
+    }
+  }
+
+
+
+}
+
+void handle_server_message(connection_info *connection)
+{
+  message msg;
+
+  //Receive a reply from the server
+  ssize_t recv_val = recv(connection->socket, &msg, sizeof(message), 0);
+  if(recv_val < 0)
+  {
+      perror("recv failed");
+      exit(1);
+
+  }
+  else if(recv_val == 0)
+  {
+    close(connection->socket);
+    puts("Server disconnected.");
+    exit(0);
+  }
+
+  switch(msg.type)
+  {
+
+    case CONNECT:
+      printf(KYEL "%s has connected." RESET "\n", msg.username);
+    break;
+
+    case DISCONNECT:
+      printf(KYEL "%s has disconnected." RESET "\n" , msg.username);
+    break;
+
+    case GET_USERS:
+      printf("%s", msg.data);
+    break;
+
+    case SET_USERNAME:
+      //TODO: implement: name changes in the future?
+    break;
+
+    case PUBLIC_MESSAGE:
+      printf(KGRN "%s" RESET ": %s\n", msg.username, msg.data);
+    break;
+
+    case PRIVATE_MESSAGE:
+      printf(KWHT "From %s:" KCYN " %s\n" RESET, msg.username, msg.data);
+    break;
+
+    case TOO_FULL:
+      fprintf(stderr, KRED "Server chatroom is too full to accept new clients." RESET "\n");
+      exit(0);
+    break;
+
+    default:
+      fprintf(stderr, KRED "Unknown message type received." RESET "\n");
+    break;
+  }
+}
+
 int main(int argc, char *argv[])
 {
-	int fd = 0;
-	char buff[1024];
+  connection_info connection;
+  fd_set file_descriptors;
 
-	if (argc < 3)
-	{
-		printf("Less no of arguments !!");
-		return 0;
-	}
+  if (argc != 3) {
+    fprintf(stderr,"Usage: %s <IP> <port>\n", argv[0]);
+    exit(1);
+  }
 
-	//Setup Buffer Array
-	memset(buff, '0', sizeof(buff));
+  connect_to_server(&connection, argv[1], argv[2]);
 
-	//Create Socket
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd < 0)
-	{
-		perror("Client Error: Socket not created succesfully");
-		return 0;
-	}
+  //keep communicating with server
+  while(true)
+  {
+    FD_ZERO(&file_descriptors);
+    FD_SET(STDIN_FILENO, &file_descriptors);
+    FD_SET(connection.socket, &file_descriptors);
+    fflush(stdin);
 
-	//Structure to store details
-	struct sockaddr_in server;
-	memset(&server, '0', sizeof(server));
+    if(select(connection.socket+1, &file_descriptors, NULL, NULL, NULL) < 0)
+    {
+      perror("Select failed.");
+      exit(1);
+    }
 
-	//Initialize
-	server.sin_family = AF_INET;
-	server.sin_port = htons(atoi(argv[2]));
+    if(FD_ISSET(STDIN_FILENO, &file_descriptors))
+    {
+      handle_user_input(&connection);
+    }
 
-	int in = inet_pton(AF_INET, argv[1], &server.sin_addr);
-	if (in < 0)
-	{
-		perror("Client Error: IP not initialized succesfully");
-		return 0;
-	}
+    if(FD_ISSET(connection.socket, &file_descriptors))
+    {
+      handle_server_message(&connection);
+    }
+  }
 
-	//Connect to given server
-	in = connect(fd, (struct sockaddr *)&server, sizeof(server));
-	if (in < 0)
-	{
-		perror("Client Error: Connection Failed.");
-		return 0;
-	}
-
-	while (1)
-	{
-		printf("Please enter the message: ");
-		bzero(buff, 256);
-		fgets(buff, 255, stdin);
-
-		printf("\nSending to SERVER: %s ", buff);
-
-		/* Send message to the server */
-		in = send(fd, buff, strlen(buff), 0);
-		if (in < 0)
-		{
-			perror("\nClient Error: Writing to Server");
-			return 0;
-		}
-
-		/* Now read server response */
-		bzero(buff, 256);
-		in = recv(fd, buff, 255, 0);
-		if (in < 0)
-		{
-			perror("\nClient Error: Reading from Server");
-			return 0;
-		}
-		printf("\nReceived FROM SERVER: %s ", buff);
-	}
-	close(fd);
-	return 0;
+  close(connection.socket);
+  return 0;
 }
+
+
+//something to look into for better stdin
+//http://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+// #include <unistd.h>
+// #include <termios.h>
+//
+// char getch() {
+//         char buf = 0;
+//         struct termios old = {0};
+//         if (tcgetattr(0, &old) < 0)
+//                 perror("tcsetattr()");
+//         old.c_lflag &= ~ICANON;
+//         old.c_lflag &= ~ECHO;
+//         old.c_cc[VMIN] = 1;
+//         old.c_cc[VTIME] = 0;
+//         if (tcsetattr(0, TCSANOW, &old) < 0)
+//                 perror("tcsetattr ICANON");
+//         if (read(0, &buf, 1) < 0)
+//                 perror ("read()");
+//         old.c_lflag |= ICANON;
+//         old.c_lflag |= ECHO;
+//         if (tcsetattr(0, TCSADRAIN, &old) < 0)
+//                 perror ("tcsetattr ~ICANON");
+//         return (buf);
+// }
--- server.c
+++ server.c
@@ -1,75 +1,399 @@
+/*
+*
+* Chatroom - a simple linux commandline client/server C program for group chat.
+* Author: Andrew Herriot
+* License: Public Domain
+*
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+#include <pthread.h>
+
+#include "chatroom_utils.h"
 #include <string.h>
-#include <sys/types.h>
 
-int main()
+void trim_newline(char *text)
 {
-    int fd = 0;
-    char buff[1024];
-    char nbuff[1024];
-    
-    //Setup Buffer Array
-    memset(buff, '0',sizeof(buff)); 
+  int len = strlen(text) - 1;
+  if (text[len] == '\n')
+{
+      text[len] = '\0';
+  }
+}
 
-    //Create Socket
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-        if(fd<0)
+void clear_stdin_buffer()
+{
+  int c;
+  while((c = getchar()) != '\n' && c != EOF)
+    /* discard content*/ ;
+}
+
+
+#define MAX_CLIENTS 4
+
+void initialize_server(connection_info *server_info, int port)
+{
+  if((server_info->socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("Failed to create socket");
+    exit(1);
+  }
+
+  server_info->address.sin_family = AF_INET;
+  server_info->address.sin_addr.s_addr = INADDR_ANY;
+  server_info->address.sin_port = htons(port);
+
+  if(bind(server_info->socket, (struct sockaddr *)&server_info->address, sizeof(server_info->address)) < 0)
+  {
+    perror("Binding failed");
+    exit(1);
+  }
+
+  const int optVal = 1;
+  const socklen_t optLen = sizeof(optVal);
+  if(setsockopt(server_info->socket, SOL_SOCKET, SO_REUSEADDR, (void*) &optVal, optLen) < 0)
+  {
+    perror("Set socket option failed");
+    exit(1);
+  }
+
+
+  if(listen(server_info->socket, 3) < 0) {
+    perror("Listen failed");
+    exit(1);
+  }
+
+  //Accept and incoming connection
+  printf("Waiting for incoming connections...\n");
+}
+
+void send_public_message(connection_info clients[], int sender, char *message_text)
+{
+  message msg;
+  msg.type = PUBLIC_MESSAGE;
+  strncpy(msg.username, clients[sender].username, 20);
+  strncpy(msg.data, message_text, 256);
+  int i = 0;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(i != sender && clients[i].socket != 0)
     {
-        perror("Client Error: Socket not created succesfully");
-        return 0;
+      if(send(clients[i].socket, &msg, sizeof(msg), 0) < 0)
+      {
+          perror("Send failed");
+          exit(1);
+      }
     }
-    //Structure to store details
-    struct sockaddr_in server; 
-    memset(&server, '0', sizeof(server)); 
+  }
+}
 
-    //Initialize    
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8000); 
-        server.sin_addr.s_addr = htonl(INADDR_ANY);
-   
+void send_private_message(connection_info clients[], int sender,
+  char *username, char *message_text)
+{
+  message msg;
+  msg.type = PRIVATE_MESSAGE;
+  strncpy(msg.username, clients[sender].username, 20);
+  strncpy(msg.data, message_text, 256);
 
-    bind(fd, (struct sockaddr*)&server, sizeof(server)); 
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(i != sender && clients[i].socket != 0
+      && strcmp(clients[i].username, username) == 0)
+    {
+      if(send(clients[i].socket, &msg, sizeof(msg), 0) < 0)
+      {
+          perror("Send failed");
+          exit(1);
+      }
+      return;
+    }
+  }
 
-    int in;
-    
-    listen(fd, 10); 
-    while(  in = accept(fd, (struct sockaddr*)NULL, NULL))
-    {       
-        int childpid,n;
-        if ( (childpid = fork ()) == 0 ) 
+  msg.type = USERNAME_ERROR;
+  sprintf(msg.data, "Username \"%s\" does not exist or is not logged in.", username);
+
+  if(send(clients[sender].socket, &msg, sizeof(msg), 0) < 0)
+  {
+      perror("Send failed");
+      exit(1);
+  }
+
+}
+
+void send_connect_message(connection_info *clients, int sender)
+{
+  message msg;
+  msg.type = CONNECT;
+  strncpy(msg.username, clients[sender].username, 21);
+  int i = 0;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(clients[i].socket != 0)
+    {
+      if(i == sender)
+      {
+        msg.type = SUCCESS;
+        if(send(clients[i].socket, &msg, sizeof(msg), 0) < 0)
         {
-        
-            //printf ("\nOne Client Connected !! ");
-        
-            //close listening socket
-            close (fd);
-        
-            //Clear Zeroes
-            bzero(buff,256);
-            bzero(nbuff,256);
-            while ( (n = recv(in, buff, 256,0)) > 0)  
-            {
-            
-                printf("Server Received: %s",buff);
-                printf("Enter your message \n");
-                //bzero(nbuff,256);
-                fgets(nbuff,255,stdin);
-                
-                //char *nbuff = strrev(buff);
-                //printf("Server Sending: %s",strrev(buff));
-                //Send the reversed input
-                send(in, nbuff, strlen(nbuff), 0);
-        
-                bzero(buff,256);
-                                        
-            }
-            close(in);
-            exit(0);
+            perror("Send failed");
+            exit(1);
         }
+      }else
+      {
+        if(send(clients[i].socket, &msg, sizeof(msg), 0) < 0)
+        {
+            perror("Send failed");
+            exit(1);
+        }
+      }
     }
+  }
+}
+
+void send_disconnect_message(connection_info *clients, char *username)
+{
+  message msg;
+  msg.type = DISCONNECT;
+  strncpy(msg.username, username, 21);
+  int i = 0;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(clients[i].socket != 0)
+    {
+      if(send(clients[i].socket, &msg, sizeof(msg), 0) < 0)
+      {
+          perror("Send failed");
+          exit(1);
+      }
+    }
+  }
+}
+
+void send_user_list(connection_info *clients, int receiver) {
+  message msg;
+  msg.type = GET_USERS;
+  char *list = msg.data;
+
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(clients[i].socket != 0)
+    {
+      list = stpcpy(list, clients[i].username);
+      list = stpcpy(list, "\n");
+    }
+  }
+
+  if(send(clients[receiver].socket, &msg, sizeof(msg), 0) < 0)
+  {
+      perror("Send failed");
+      exit(1);
+  }
+
+}
+
+void send_too_full_message(int socket)
+{
+  message too_full_message;
+  too_full_message.type = TOO_FULL;
+
+  if(send(socket, &too_full_message, sizeof(too_full_message), 0) < 0)
+  {
+      perror("Send failed");
+      exit(1);
+  }
+
+  close(socket);
+}
+
+//close all the sockets before exiting
+void stop_server(connection_info connection[])
+{
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    //send();
+    close(connection[i].socket);
+  }
+  exit(0);
+}
+
+
+void handle_client_message(connection_info clients[], int sender)
+{
+  int read_size;
+  message msg;
+
+  if((read_size = recv(clients[sender].socket, &msg, sizeof(message), 0)) == 0)
+  {
+    printf("User disconnected: %s.\n", clients[sender].username);
+    close(clients[sender].socket);
+    clients[sender].socket = 0;
+    send_disconnect_message(clients, clients[sender].username);
+
+  } else {
+
+    switch(msg.type)
+    {
+      case GET_USERS:
+        send_user_list(clients, sender);
+      break;
+
+      case SET_USERNAME: ;
+        int i;
+        for(i = 0; i < MAX_CLIENTS; i++)
+        {
+          if(clients[i].socket != 0 && strcmp(clients[i].username, msg.username) == 0)
+          {
+            close(clients[sender].socket);
+            clients[sender].socket = 0;
+            return;
+          }
+        }
+
+        strcpy(clients[sender].username, msg.username);
+        printf("User connected: %s\n", clients[sender].username);
+        send_connect_message(clients, sender);
+      break;
+
+      case PUBLIC_MESSAGE:
+        send_public_message(clients, sender, msg.data);
+      break;
+
+      case PRIVATE_MESSAGE:
+        send_private_message(clients, sender, msg.username, msg.data);
+      break;
+
+      default:
+        fprintf(stderr, "Unknown message type received.\n");
+      break;
+    }
+  }
+}
+
+int construct_fd_set(fd_set *set, connection_info *server_info,
+                      connection_info clients[])
+{
+  FD_ZERO(set);
+  FD_SET(STDIN_FILENO, set);
+  FD_SET(server_info->socket, set);
+
+  int max_fd = server_info->socket;
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(clients[i].socket > 0)
+    {
+      FD_SET(clients[i].socket, set);
+      if(clients[i].socket > max_fd)
+      {
+        max_fd = clients[i].socket;
+      }
+    }
+  }
+  return max_fd;
+}
+
+void handle_new_connection(connection_info *server_info, connection_info clients[])
+{
+  int new_socket;
+  int address_len;
+  new_socket = accept(server_info->socket, (struct sockaddr*)&server_info->address, (socklen_t*)&address_len);
+
+  if (new_socket < 0)
+  {
+    perror("Accept Failed");
+    exit(1);
+  }
+
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    if(clients[i].socket == 0) {
+      clients[i].socket = new_socket;
+      break;
+
+    } else if (i == MAX_CLIENTS -1) // if we can accept no more clients
+    {
+      send_too_full_message(new_socket);
+    }
+  }
+}
+
+void handle_user_input(connection_info clients[])
+{
+  char input[255];
+  fgets(input, sizeof(input), stdin);
+  trim_newline(input);
+
+  if(input[0] == 'q') {
+    stop_server(clients);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  puts("Starting server.");
+
+  fd_set file_descriptors;
+
+  connection_info server_info;
+  connection_info clients[MAX_CLIENTS];
+
+  int i;
+  for(i = 0; i < MAX_CLIENTS; i++)
+  {
+    clients[i].socket = 0;
+  }
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  initialize_server(&server_info, atoi(argv[1]));
+
+  while(true)
+  {
+    int max_fd = construct_fd_set(&file_descriptors, &server_info, clients);
+
+    if(select(max_fd+1, &file_descriptors, NULL, NULL, NULL) < 0)
+    {
+      perror("Select Failed");
+      stop_server(clients);
+    }
+
+    if(FD_ISSET(STDIN_FILENO, &file_descriptors))
+    {
+      handle_user_input(clients);
+    }
+
+    if(FD_ISSET(server_info.socket, &file_descriptors))
+    {
+      handle_new_connection(&server_info, clients);
+    }
+
+    for(i = 0; i < MAX_CLIENTS; i++)
+    {
+      if(clients[i].socket > 0 && FD_ISSET(clients[i].socket, &file_descriptors))
+      {
+        handle_client_message(clients, i);
+      }
+    }
+  }
+
+  return 0;
 }
